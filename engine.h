9cccda82deb70f9970aeab9d156fe4a9c8db6702
parham <bghparham@gmail.com>
2019-01-21 22:33:15 +0330

    vectorize and merge with Board

--- engine.h
+++ engine.h
@@ -108,8 +108,7 @@ class Engine{
 private:
 	Rule * rule;
 	Board * board;
-	Player ** players;
-	int nPlayer;
+	vector<Player *> players;
 	UIConnection * ui;
 public:
 	Event giveMyMove(int index);
