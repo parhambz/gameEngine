37dfadb8098895ae7280e79e26a70e3e5dde935d
unknown <vaziri.aroo1377@gmail.com>
2019-01-21 22:56:56 +0330

    in event : * => & , playerTurn Function added ! , implement complete

---
+++ Event.cpp
@@ -0,0 +1,24 @@
+#include "engine.h"
+
+
+Event::Event(Pair pair, Player& player) :loacation(pair), player(player) {}
+	
+
+
+
+ Pair Event::getLocation() {
+
+	return loacation;
+}
+
+ Player& Event::getPlayer() {
+	 return player;
+}
+
+ void Event::setLocation(Pair p) {
+	 loacation = p;
+}
+
+ void Event::setPlayer(Player& player) {
+	 this->player = player;
+ }
