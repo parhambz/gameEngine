99068b4986960741b80c5e6ef4dd26e9f2841fcd
ijinima <ijinima@gmail.com>
2019-01-22 23:59:32 +0330

    start

--- src/xo/Main.java
+++ src/xo/Main.java
@@ -4,16 +4,31 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+
+        //Making Stage
+        makingStage(primaryStage, root, "Game", 500, 500);
+
+
+
+        //show stage
         primaryStage.show();
+    }
+
+    public static void makingStage(Stage primaryStage, Parent root, String title, int width, int height) {
+        primaryStage.setTitle(title);
+        Scene scene = new Scene(root,width,height);
+        scene.setFill(Color.BLUE);
+        primaryStage.setScene(scene);
+
     }
 
 
---
+++ src/xo/MenuUi.java
@@ -0,0 +1,7 @@
+package xo;
+
+/**
+ * Created by nima on 1/22/2019 AD.
+ */
+public class MenuUi  {
+}
