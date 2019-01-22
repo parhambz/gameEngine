4804f6051565c4ff9370bf04740f7036c7dd0a89
ijinima <ijinima@gmail.com>
2019-01-23 00:14:47 +0330

    making MenuUi.java

--- src/xo/MenuUi.java
+++ src/xo/MenuUi.java
@@ -1,7 +1,25 @@
 package xo;
 
-/**
- * Created by nima on 1/22/2019 AD.
- */
-public class MenuUi  {
+
+import javafx.collections.ObservableList;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+
+
+public class MenuUi extends VBox {
+
+    private double SPACE_SIZE = 10;
+    ObservableList list;
+
+    MenuUi(){
+        list = this.getChildren();
+        this.setSpacing(SPACE_SIZE);
+        System.out.println("Menu Created!");
+    }
+
+    public void addButton(Button button){
+        list.add(button);
+        System.out.println(button.getText().toString() + " Button Added to Menu!");
+    }
+
 }
