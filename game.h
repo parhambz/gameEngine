3bfbed3fc94eba531eb6d074d27289f7c872edbc
parham <bghparham@gmail.com>
2019-01-23 12:51:26 +0330

    game.h created

---
+++ game.h
@@ -0,0 +1,14 @@
+//
+// Created by Parham Bagherzadeh on 1/23/2019 AD.
+//
+
+#ifndef GAMEENGINE_GAME_H
+#define GAMEENGINE_GAME_H
+
+#endif //GAMEENGINE_GAME_H
+
+#include "engine.h"
+
+class DoozBoard :public Board{
+
+};
