4b9549d9716d08c91b5d9a8df0dc577c3db417fb
parham <bghparham@gmail.com>
2019-01-24 19:46:21 +0330

    jni done but func isOver ....

--- xo_UiConnection.cpp
+++ xo_UiConnection.cpp
@@ -11,11 +11,12 @@ JNIEXPORT jobject JNICALL Java_xo_UiConnection_start
     eng->setBoard(new DoozBoard(Pair(x,y)));
     eng->setRule(new DoozRule);
     eng->setGameStruct(x,y,name1,name2,isAuto1,isAuto2);
+    eng->setPlayers();
     eng->start();
 }
 
 JNIEXPORT jobject JNICALL Java_xo_UiConnection_botMove(JNIEnv *, jobject){
-
+    return eng->botMove();
 }
 JNIEXPORT jobject JNICALL Java_xo_UiConnection_end
         (JNIEnv *, jobject){
